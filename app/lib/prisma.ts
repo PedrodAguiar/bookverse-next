9b086bcba07716ca0d7cce81835d72b68454053c
PedrodAguiar <pedrodaniel8416@gmail.com>
2026-03-11 09:49:24 -0300

    fix: atualização do Prisma Client para usar adapter do Neon

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from "@prisma/client"
+import { PrismaNeon } from "@prisma/adapter-neon"
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-export const prisma = globalThis.prisma ?? new PrismaClient()
+export const prisma =
+  globalThis.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = prisma
